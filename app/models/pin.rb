b419805a84bd842c8c9d7aa37d1bf75510cc8905
d-ugol <d.ugol@comcast.net>
2018-06-07 11:11:41 -0400

    updated gemfile for AWS

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,8 +4,15 @@ class Pin < ApplicationRecord
   # D.Ugol it's a good idea to have a placeholder for a missing image. 
   # E.g.   default_url: "/images/:style/missing.png"
   # This is a note for future enhancements.
+  # The ">" and "#" symbols tell ImageMagick how to resize the image. ">" means proportional resizing.
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { 
+  	medium: "300x300>", 
+  	square: "200x200#",
+  	thumb: "100x100>" 
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc.
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   # D.Ugol description is mandatory
